439c684c7568375e9a52ab12ce689d3d71e8f15e
Daisky <nakamura.daisky@gmail.com>
2021-05-03 12:02:10 +0900

    画面とAudioControllerの関連付け

--- js/index.js
+++ js/index.js
@@ -1,77 +1,17 @@
 //----------------------------------------
+// audio
+//----------------------------------------
+const audioController = new AudioController()
+//----------------------------------------
+// graphic
+//----------------------------------------
+// const graphicController = new GraphicController()
+//----------------------------------------
 // dom
 //----------------------------------------
 const DISABLE_CLASS_NAME = 'inVisible'
 const OPACITY_ZERO = 'opacity-zero'
-
-const playBtn = document.querySelector('.js-play')
-const pauseBtn = document.querySelector('.js-pause')
-const volumeBtn = document.querySelector('.js-volumeUp')
-const muteBtn = document.querySelector('.js-volumeMute')
-const inputs = {
-    volume: document.querySelector('#js-volumeInput'),
-    lowpath: document.querySelector('#js-lowPathInput'),
-    lowpath: document.querySelector('#js-midPathInput'),
-    lowpath: document.querySelector('#js-highPathInput'),
-    lowpath: document.querySelector('#js-bpmInput'),
-}
-
 let volume = 50
-
-console.log(muteBtn)
-
-playBtn.addEventListener('click', function () {
-    playBtn.classList.add(DISABLE_CLASS_NAME)
-    pauseBtn.classList.remove(DISABLE_CLASS_NAME)
-})
-
-pauseBtn.addEventListener('click', function () {
-    playBtn.classList.remove(DISABLE_CLASS_NAME)
-    pauseBtn.classList.add(DISABLE_CLASS_NAME)
-})
-
-volumeBtn.addEventListener('click', function () {
-    volumeBtn.classList.add(DISABLE_CLASS_NAME)
-    muteBtn.classList.remove(DISABLE_CLASS_NAME)
-    inputs.volume.value = 0
-    inputs.volume.style.background = `
-         linear-gradient(to right, #eee 0%, #eee 100%)
-         `
-})
-
-muteBtn.addEventListener('click', function () {
-    volumeBtn.classList.remove(DISABLE_CLASS_NAME)
-    muteBtn.classList.add(DISABLE_CLASS_NAME)
-    inputs.volume.value = volume
-    inputs.volume.style.background = `
-         linear-gradient(to right, #66BAB7 0%, #66BAB7 ${volume}%, #eee ${volume}%, #eee 100%)
-        `
-})
-
-const inputRange = Array.from(document.querySelectorAll('.js-inputRange')).forEach(el => {
-    el.addEventListener('input', function (e) {
-        e.target.style.background = `
-         linear-gradient(to right, #66BAB7 0%, #66BAB7 ${el.value}%, #eee ${el.value}%, #eee 100%)
-         `
-        // console.log(e.target.value)
-    })
-})
-
-const inputVertical = Array.from(document.querySelectorAll('.js-inputVerticalWrapper')).forEach(el => {
-    const input = el.querySelector('.js-inputRange')
-    const inputValArea = el.querySelector('.val')
-    input.addEventListener('input', function (e) {
-        inputValArea.classList.remove(OPACITY_ZERO)
-        inputValArea.textContent = e.target.value
-    })
-    input.addEventListener('change', function () {
-        setTimeout(() => {
-            inputValArea.classList.add(OPACITY_ZERO)
-        }, 1000)
-    })
-
-})
-
 const h1 = new MyShuffleText({
     el: document.querySelector('#js-headLine'),
     duration: 1000,
@@ -80,45 +20,125 @@ const h1 = new MyShuffleText({
     fps: 60
 })
 
+const p = new MyShuffleText({
+    el: document.querySelector('#js-paragraph'),
+    duration: 1000,
+    hideDuration: 1000,
+    delay: 300,
+    fps: 60
+})
 
+const playBtn = document.querySelector('.js-play')
+const pauseBtn = document.querySelector('.js-pause')
+const volumeBtn = document.querySelector('.js-volumeUp')
+const muteBtn = document.querySelector('.js-volumeMute')
 const fileInputWrapper = document.querySelector('.fileInput-wrapper')
 
+const inputs = {
+    file: document.querySelector('#js-fileInput'),
+    range: {
+        horizonatal: {
+            volume: document.querySelector('#js-volumeInput'),
+        },
+        verticalWrapper: Array.from(document.querySelectorAll('.js-inputVerticalWrapper'))
+    }
+}
+
+
+const setInputRangeStyle = (target, val) => {
+    target.style.background = `
+         linear-gradient(to right, #66BAB7 0%, #66BAB7 ${val}%, #eee ${val}%, #eee 100%)
+        `
+}
+
+handleBtnEvnts = () => {
+    playBtn.addEventListener('click', function () {
+        playBtn.classList.add(DISABLE_CLASS_NAME)
+        pauseBtn.classList.remove(DISABLE_CLASS_NAME)
+        audioController.play()
+    })
+
+    pauseBtn.addEventListener('click', function () {
+        playBtn.classList.remove(DISABLE_CLASS_NAME)
+        pauseBtn.classList.add(DISABLE_CLASS_NAME)
+        audioController.pause()
+    })
+
+    volumeBtn.addEventListener('click', function () {
+        volumeBtn.classList.add(DISABLE_CLASS_NAME)
+        muteBtn.classList.remove(DISABLE_CLASS_NAME)
+        inputs.range.horizonatal.volume.value = 0
+        setInputRangeStyle(inputs.range.horizonatal.volume, 0)
+    })
+
+    muteBtn.addEventListener('click', function () {
+        volumeBtn.classList.remove(DISABLE_CLASS_NAME)
+        muteBtn.classList.add(DISABLE_CLASS_NAME)
+        inputs.range.horizonatal.volume.value = volume
+        setInputRangeStyle(inputs.range.horizonatal.volume, volume)
+    })
+}
+
+const handleInputEvents = () => {
+
+    inputs.range.horizonatal.volume.addEventListener('input', e => {
+        setInputRangeStyle(e.target, e.target.value)
+    })
+
+    inputs.range.verticalWrapper.forEach(el => {
+        const valArea = el.querySelector('.val')
+        const input = el.querySelector('.js-inputRange')
+        input.addEventListener('input', e => {
+            valArea.classList.remove(OPACITY_ZERO)
+            setInputRangeStyle(e.target, e.target.value)
+            valArea.textContent = e.target.value
+        })
+
+        input.addEventListener('change', () => {
+            setTimeout(() => {
+                valArea.classList.add(OPACITY_ZERO)
+            }, 1000)
+        })
+    })
+}
+
+const handleFileEvent = () => {
+    inputs.file.oninput = async e => {
+        const file = e.target.files[0]
+        const trackInfo = trimTrackInfo(file.name)
+        const trackName = trackInfo?.trackName || foo
+        const creatorName = trackInfo?.creator || bar
+        h1.resetAndShow(trackName)
+        p.resetAndShow(creatorName)
+        audioController.loadTrack(file)
+    }
+}
+
+const handleEvents = () => {
+    handleBtnEvnts()
+    handleInputEvents()
+    handleFileEvent()
+}
+
+handleEvents()
+
+
+const trimTrackInfo = text => {
+    const splitText = text.split(/\.|\-/)
+    return {
+        trackName: splitText[0],
+        creator: splitText[1]
+    }
+}
+
+
+
 h1.show()
-    .then(() => fileInputWrapper.classList.remove('inVisible'))
+    .then(() => p.show())
+    .then(() => fileInputWrapper.classList.remove(DISABLE_CLASS_NAME))
 
 
-// const lorem = 'Hello MyshuffleText The World'
-// h1.resetAndShow(lorem)
-//     .then(() => h1.hide())
-//     .then(() => h1.show())
-//     .then(() => h1.show())
-//     .then(() => h1.hide())
-//     .then(() => h1.resetAndShow('lorem ipsum lorem ipsum'))
 
-    // .then(() => h1.resetAndShow('bar'))
-    // .then(() => h1.resetAndShow('hoge'))
-    // .then(() => h1.resetAndShow('Hello MyShuffleText'))
-    // .then(() => {
-    //     fileInputWrapper.classList.remove('inVisible')
-    //     // fileInputWrapper.classList.add('visible')
-    // })
-// .then(() => h1.hide())
-// .then(() => h1.resetAndShow('Hello'))
-
-//----------------------------------------
-// audio
-//----------------------------------------
-// const audioController = new AudioController({
-//     fileInput: '#js-fileInput',
-//     playBtn: '#js-playBtn',
-//     pauseBtn: '#js-pauseBtn',
-//     volume: '#js-volumeController',
-//     trackNameArea: '#js-trackName'
-// })
-//----------------------------------------
-// graphic
-//----------------------------------------
-// const graphicController = new GraphicController()
 
 //----------------------------------------
 // animation
