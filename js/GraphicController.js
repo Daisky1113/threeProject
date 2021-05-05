baa99d3ce105ad9b6406ea245c5b3b384909a372
Daisky <nakamura.daisky@gmail.com>
2021-05-06 07:16:45 +0900

    geometryとmaterialをクラスのプロパティとして定義

--- js/GraphicController.js
+++ js/GraphicController.js
@@ -15,6 +15,8 @@ class GraphicController {
         this.round = []
         this.fixMesh = []
         this.roundRadius = 500
+        this.sphereGeometry = new THREE.SphereGeometry(5, 32, 32)
+        this.material = new THREE.MeshBasicMaterial()
         this.init()
     }
 
@@ -125,16 +127,15 @@ class GraphicController {
     }
 
     createRound(length, r) {
-        const geometry = new THREE.SphereGeometry(5, 32, 32);
         const baseDig = 360 / length
         const baseColor = 1 / length
         for (let i = 0; i < length; i++) {
             const dig = baseDig * (i + 1)
             const rad = dig * Math.PI / 180
             const color = baseColor * i
-            const material = new THREE.MeshBasicMaterial()
+            const material = this.material
             material.color.setHSL(color, 0.98, 0.98)
-            const mesh = new THREE.Mesh(geometry, material)
+            const mesh = new THREE.Mesh(this.sphereGeometry, material)
             mesh.position.x = Math.cos(rad) * this.roundRadius
             mesh.position.z = Math.sin(rad) * this.roundRadius
             this.round.push(mesh)
@@ -161,21 +162,15 @@ class GraphicController {
 
     updateRoundScale(prevData, data) {
         this.round.forEach((mesh, i) => {
-            mesh.scale.x = data[i] / 100
-            mesh.scale.y = data[i] / 100
-            mesh.scale.z = data[i] / 100
             if (data[i] > 126 && data[i] > prevData[i] * 1.2) {
-                const geometry = new THREE.SphereGeometry(5, 32, 32);
-                const material = new THREE.MeshBasicMaterial()
+                const material = this.material
                 const meshHSL = mesh.material.color.getHSL({})
                 material.color.setHSL(meshHSL.h, data[i] / 256, meshHSL.l - Math.random() * Math.random() * Math.random() * Math.random())
-                const copyMesh = new THREE.Mesh(geometry, material)
-                copyMesh.position.x = mesh.position.x * Math.random()
-                copyMesh.position.y = mesh.position.y * Math.random()
-                copyMesh.position.z = mesh.position.z * Math.random()
-                copyMesh.scale.x = mesh.scale.x
-                copyMesh.scale.y = mesh.scale.y
-                copyMesh.scale.z = mesh.scale.z
+                const copyMesh = new THREE.Mesh(this.sphereGeometry, material)
+                copyMesh.position.x = mesh.position.x * Math.random() * Math.random()
+                copyMesh.position.y = mesh.position.y * Math.random() * Math.random()
+                copyMesh.position.z = mesh.position.z * Math.random() * Math.random()
+
                 this.scene.add(copyMesh)
                 setTimeout(() => {
                     this.scene.remove(copyMesh)
