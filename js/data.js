1ec0bec76240420087e088ac5a6fae181f464665
Daisky <nakamura.daisky@gmail.com>
2021-05-10 21:25:18 +0900

    データ用意

---
+++ js/data.js
@@ -0,0 +1,163 @@
+const members = [
+    {
+        id: 0,
+        name: '加納美織里',
+        img: 'img0.png'
+    },
+    {
+        id: 1,
+        name: '石田喜子',
+        img: 'img1.png'
+    },
+    {
+        id: 2,
+        name: '今村有希',
+        img: 'img2.png'
+    },
+    {
+        id: 3,
+        name: '岡田尚子',
+        img: 'img3.png'
+    },
+    {
+        id: 4,
+        name: '加藤瑞樹',
+        img: 'img4.png'
+    },
+    {
+        id: 5,
+        name: '川口慧大',
+        img: 'img5.png'
+    },
+    {
+        id: 6,
+        name: '神⽥ 圭輔',
+        img: 'img6.png'
+    },
+    {
+        id: 7,
+        name: 'クー・カイシアン',
+        img: 'img7.png'
+    },
+    {
+        id: 8,
+        name: '⼩⽟ ⾝師',
+        img: 'img8.png'
+    },
+    {
+        id: 8,
+        name: '小林純',
+        img: 'img9.png'
+    },
+    {
+        id: 9,
+        name: '小林純',
+        img: 'img9.png'
+    },
+    {
+        id: 10,
+        name: '清水章宏',
+        img: 'img10.png'
+    },
+    {
+        id: 11,
+        name: '⽩⽯還',
+        img: 'img11.png'
+    },
+    {
+        id: 12,
+        name: '白鳥陽太郎',
+        img: 'img12.png'
+    },
+    {
+        id: 13,
+        name: '菅原舞子',
+        img: 'img13.png'
+    },
+    {
+        id: 14,
+        name: '関口航',
+        img: 'img14.png'
+    },
+    {
+        id: 15,
+        name: '髙橋 直樹',
+        img: 'img15.png'
+    },
+    {
+        id: 16,
+        name: '瀧田凱斗',
+        img: 'img16.png'
+    },
+    {
+        id: 17,
+        name: '坪川耀一',
+        img: 'img17.png'
+    },
+    {
+        id: 18,
+        name: '中尾哲朗',
+        img: 'img18.png'
+    },
+    {
+        id: 19,
+        name: '中村拓磨',
+        img: 'img19.png'
+    },
+    {
+        id: 20,
+        name: '能祖靖夫',
+        img: 'img20.png'
+    },
+    {
+        id: 21,
+        name: '舟木純',
+        img: 'img21.png'
+    },
+    {
+        id: 22,
+        name: '保坂映里',
+        img: 'img22.png'
+    },
+    {
+        id: 23,
+        name: '宮本さくら',
+        img: 'img23.png'
+    },
+    {
+        id: 24,
+        name: '和多田匡孝',
+        img: 'img24.png'
+    },
+    {
+        id: 25,
+        name: '柳川佑平',
+        img: 'img25.png'
+    },
+    {
+        id: 26,
+        name: '上野 隼平',
+        img: 'img26.png'
+    },
+    {
+        id: 27,
+        name: '加來真有 ',
+        img: 'img27.png'
+    },
+    {
+        id: 28,
+        name: '前田達也',
+        img: 'img28.jpg'
+    },
+    {
+        id: 29,
+        name: 'DaiskyNakamura',
+        img: 'img29.jpg'
+    },
+    {
+        id: 30,
+        name: '学校長 山崎 ',
+        img: 'img30.jpg'
+    },
+
+]
